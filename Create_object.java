505d52ae42d5b7d7853d40b99248a0eb4a964113
rakeshpurata <rakeshpurata123@gmail.com>
2024-08-02 00:28:22 +0530

    Create Create_object.java

---
+++ Create_object.java
@@ -0,0 +1,14 @@
+public class CreateObjectExample
+{    
+void show()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{    
+  
+CreateObjectExample obj = new CreateObjectExample();   
+
+obj.show();    
+}    
+}
